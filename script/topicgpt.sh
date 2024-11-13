f0705e77714921ced4f2db14126937751d9687a3
Lucy Li <lucy@tahoe.ischool.berkeley.edu>
2024-11-13 09:24:13 -0800

    prompts and script

---
+++ script/topicgpt.sh
@@ -0,0 +1,33 @@
+# CUDA_VISIBLE_DEVICES=0,1 python generation_1.py --deployment_name Phi-3.5-mini \
+#                         --max_tokens 300 --temperature 0.0 --top_p 0.0 \
+#                         --data ../data/input/generate_passages.jsonl \
+#                         --prompt_file ../prompt/generation_1.txt \
+#                         --seed_file ../prompt/seed_1.md \
+#                         --out_file ../data/output/generation_phi.jsonl \
+#                         --topic_file ../data/output/generation_phi.md \
+#                         --verbose True
+
+# CUDA_VISIBLE_DEVICES=0,1 python generation_1.py --deployment_name Llama-3.1-8B \
+#                         --max_tokens 300 --temperature 0.0 --top_p 0.0 \
+#                         --data ../data/input/generate_passages.jsonl \
+#                         --prompt_file ../prompt/generation_1.txt \
+#                         --seed_file ../prompt/seed_1.md \
+#                         --out_file ../data/output/generation_llama.jsonl \
+#                         --topic_file ../data/output/generation_llama.md \
+#                         --verbose False
+
+CUDA_VISIBLE_DEVICES=0,1 python assignment.py --deployment_name Phi-3.5-mini \
+                        --max_tokens 300 --temperature 0.0 --top_p 0.0 \
+                        --data ../data/input/assignment_passages.jsonl \
+                        --prompt_file ../prompt/assignment.txt \
+                        --topic_file ../data/output/generation_phi.md \
+                        --out_file ../data/output/assignment_phi.jsonl \
+                        --verbose True
+
+# CUDA_VISIBLE_DEVICES=0,1 python assignment.py --deployment_name Llama-3.1-8B \
+#                         --max_tokens 300 --temperature 0.0 --top_p 0.0 \
+#                         --data ../data/input/assignment_passages.jsonl \
+#                         --prompt_file ../prompt/assignment.txt \
+#                         --topic_file ../data/output/generation_llama.md \
+#                         --out_file ../data/output/assignment_llama.jsonl \
+#                         --verbose True
