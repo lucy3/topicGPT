16d5754ff4773029fb4b78d5906948a0fc55bd58
Lucy Li <lucy@tahoe.ischool.berkeley.edu>
2025-01-10 14:06:05 -0800

    llama case study runs

--- script/topicgpt.sh
+++ script/topicgpt.sh
@@ -7,23 +7,31 @@
 #                         --topic_file ../data/output/generation_phi.md \
 #                         --verbose True
 
-#CUDA_VISIBLE_DEVICES=1,2 python generation_1.py --deployment_name Llama-3.1-8B \
+#CUDA_VISIBLE_DEVICES=1 python generation_1.py --deployment_name Llama-3.1-8B \
+#                        --max_tokens 300 --temperature 0.0 --top_p 0.0 \
+#                        --data ../data/input/cs_generate_passages.jsonl \
+#                        --prompt_file ../prompt/generation_1.txt \
+#                        --seed_file ../prompt/seed_1.md \
+#                        --out_file ../data/output/cs_generation_llama.jsonl \
+#                        --topic_file ../data/output/cs_generation_llama.md \
+#                        --verbose True
+
+# CUDA_VISIBLE_DEVICES=0 python generation_1.py --deployment_name gemma-2-2b \
 #                        --max_tokens 300 --temperature 0.0 --top_p 0.0 \
 #                        --data ../data/input/generate_passages.jsonl \
 #                        --prompt_file ../prompt/generation_1.txt \
 #                        --seed_file ../prompt/seed_1.md \
-#                        --out_file ../data/output/generation_llama.jsonl \
-#                        --topic_file ../data/output/generation_llama.md \
+#                        --out_file ../data/output/generation_gemma.jsonl \
+#                        --topic_file ../data/output/generation_gemma.md \
 #                        --verbose True
 
-CUDA_VISIBLE_DEVICES=0 python generation_1.py --deployment_name gemma-2-2b \
-                       --max_tokens 300 --temperature 0.0 --top_p 0.0 \
-                       --data ../data/input/generate_passages.jsonl \
-                       --prompt_file ../prompt/generation_1.txt \
-                       --seed_file ../prompt/seed_1.md \
-                       --out_file ../data/output/generation_gemma.jsonl \
-                       --topic_file ../data/output/generation_gemma.md \
-                       --verbose True
+#CUDA_VISIBLE_DEVICES=0 python assignment.py --deployment_name gemma-2-2b \
+#                        --max_tokens 300 --temperature 0.0 --top_p 0.0 \
+#                        --data ../data/input/assignment_passages.jsonl \
+#                        --prompt_file ../prompt/assignment.txt \
+#                        --topic_file ../data/output/generation_gemma.md \
+#                        --out_file ../data/output/assignment_gemma.jsonl \
+#                        --verbose True
 
 # python generation_1.py --deployment_name gpt-4o-mini \
 #                         --max_tokens 300 --temperature 0.0 --top_p 0.0 \
@@ -51,13 +59,13 @@ CUDA_VISIBLE_DEVICES=0 python generation_1.py --deployment_name gemma-2-2b \
 #                         --out_file ../data/output/assignment_phi.jsonl \
 #                         --verbose True
 
-# CUDA_VISIBLE_DEVICES=1,2 python assignment.py --deployment_name Llama-3.1-8B \
-#                         --max_tokens 300 --temperature 0.0 --top_p 0.0 \
-#                         --data ../data/input/assignment_passages.jsonl \
-#                         --prompt_file ../prompt/assignment.txt \
-#                         --topic_file ../data/output/generation_llama.md \
-#                         --out_file ../data/output/assignment_llama.jsonl \
-#                         --verbose True
+CUDA_VISIBLE_DEVICES=1 python assignment.py --deployment_name Llama-3.1-8B \
+                         --max_tokens 300 --temperature 0.0 --top_p 0.0 \
+                         --data ../data/input/cs_assignment_passages.jsonl \
+                         --prompt_file ../prompt/assignment.txt \
+                         --topic_file ../data/output/cs_generation_llama.md \
+                         --out_file ../data/output/cs_assignment_llama.jsonl \
+                         --verbose True
 
 # CUDA_VISIBLE_DEVICES=0,1 python assignment.py --deployment_name gpt-4o-mini \
 #                         --max_tokens 300 --temperature 0.0 --top_p 0.0 \
